f7bb76ba537525d10efba798a834aca7759ac9d5
brainfair <autoexecbat@mail.ru>
2019-07-10 17:40:49 +0300

    Update powershells.ps1

--- powershells.ps1
+++ powershells.ps1
@@ -3,3 +3,10 @@ Update-MailboxDatabaseCopy DB05\EX2016SRV2 -CatalogOnly -BeginSeed
 
 #search admin audit log
 search-adminauditlog -cmdlets set-mailbox -objectids megauser -startdate "01/01/2019" -enddate "06/21/2019" | foreach {$_.caller; $_.rundate ; $_.cmdletparameters} | fl
+
+#mailboxdatabase size statistics
+##simple
+Get-MailboxDatabase -Status | select Name,DatabaseSize,AvailableNewMailboxSpace
+##formated
+Get-MailboxDatabase -Status | sort name | select name,@{Name='DB Size (Gb)';Expression={$_.DatabaseSize.ToGb()}},@{Name='Available New Mbx Space Gb)';Expression={$_.AvailableNewMailboxSpace.ToGb()}}
+
